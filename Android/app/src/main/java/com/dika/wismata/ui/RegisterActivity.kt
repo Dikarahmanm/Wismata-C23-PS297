7bfc930ec00b0737819a4a80d60799914d7be3da
Amar Suhendra <amarsuhendra2@gmail.com>
2023-06-03 10:07:03 +0700

    feat: register layout

---
+++ Android/app/src/main/java/com/dika/wismata/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.dika.wismata.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dika.wismata.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
