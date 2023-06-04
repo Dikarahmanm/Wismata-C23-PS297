12d005c948786d88a4c59ea0c13ba9c23dad500b
Amar Suhendra <amarsuhendra2@gmail.com>
2023-06-04 17:09:29 +0700

    fix: login and register activity

--- Android/app/src/main/java/com/dika/wismata/ui/LoginActivity.kt
+++ Android/app/src/main/java/com/dika/wismata/ui/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.dika.wismata.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dika.wismata.R
+import com.dika.wismata.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- Android/app/src/main/java/com/dika/wismata/ui/RegisterActivity.kt
+++ Android/app/src/main/java/com/dika/wismata/ui/RegisterActivity.kt
@@ -1,12 +1,30 @@
 package com.dika.wismata.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dika.wismata.R
+import com.dika.wismata.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = getString(R.string.Register)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
