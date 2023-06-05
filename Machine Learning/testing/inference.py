bcbd2e9cc5d49fab013c3e47bda0592d8680d14d
AbdulHafidh-AI <abdul_h20@mhs.unsyiah.ac.id>
2023-06-05 11:45:44 +0700

    feat: add new data

---
+++ Machine Learning/testing/inference.py
@@ -0,0 +1,29 @@
+import os
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+
+model_path = "../model/filtering_model.tflite"
+
+
+class Interpreter():
+    def __init__(self, model_path):
+        self.interpreter = tf.lite.Interpreter(model_path=model_path)
+        self.interpreter.allocate_tensors()
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+
+    def predict(self, input_data):
+        self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
+        self.interpreter.invoke()
+        output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
+        return np.argmax(output_data)
+
+
+
+
+
+    
+
+
