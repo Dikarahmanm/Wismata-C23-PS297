007df6cc529df77cca19b32daf4be355983a7c0b
AbdulHafidh-AI <abdul_h20@mhs.unsyiah.ac.id>
2023-06-06 03:01:19 +0700

    fix: fix in data processing to improve model performance

--- Machine Learning/api/app.py
+++ Machine Learning/api/app.py
@@ -1,7 +1,17 @@
 import requests
+import json
 
 url = "http://127.0.0.1:5000/api/get_rating"
 
-r = requests.get(url, params={"category": 1, "price": 4})
+
+f = open('test.json')
+
+data = json.load(f)
+
+category = data['category']
+
+price = data['price']
+
+r = requests.get(url, params={"category": category, "price": price})
 
 print(r.json())
