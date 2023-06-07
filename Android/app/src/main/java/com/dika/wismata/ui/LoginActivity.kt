abb929918f49dbf5923800189eb51ded287ed1e5
Amar Suhendra <amarsuhendra2@gmail.com>
2023-06-07 14:47:55 +0700

    fix: layout validation

--- Android/app/src/main/java/com/dika/wismata/ui/LoginActivity.kt
+++ Android/app/src/main/java/com/dika/wismata/ui/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.dika.wismata.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.dika.wismata.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -17,5 +19,46 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+        customComponent()
+    }
+
+    private fun enableButton(status: Boolean) {
+        binding.btnLogin.isEnabled = status
+    }
+
+    private fun customComponent() {
+        binding.editTextEmailAddress.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val emailPattern = Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
+                val email = binding.editTextEmailAddress.text.toString().trim()
+                val password = binding.editTextPassword.text.toString().trim()
+                if (email.isNotEmpty() && email.matches(emailPattern) && password.isNotEmpty() && password.length >= 8) {
+                    enableButton(true)
+                } else {
+                    enableButton(false)
+                }
+            }
+        })
+
+        binding.editTextPassword.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val emailPattern = Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
+                val email = binding.editTextEmailAddress.text.toString().trim()
+                val password = binding.editTextPassword.text.toString().trim()
+                if (email.isNotEmpty() && email.matches(emailPattern) && password.isNotEmpty() && password.length >= 8) {
+                    enableButton(true)
+                } else {
+                    enableButton(false)
+                }
+            }
+        })
     }
 }
--- Android/app/src/main/java/com/dika/wismata/ui/RegisterActivity.kt
+++ Android/app/src/main/java/com/dika/wismata/ui/RegisterActivity.kt
@@ -3,6 +3,8 @@ package com.dika.wismata.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.core.graphics.drawable.toDrawable
 import com.dika.wismata.R
 import com.dika.wismata.databinding.ActivityRegisterBinding
@@ -23,10 +25,114 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        customComponent()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
+    }
+
+    private fun enableButton(status: Boolean) {
+        binding.btnRegister.isEnabled = status
+    }
+
+    private fun customComponent() {
+
+        binding.editTextFirstName.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+
+                val firstName = binding.editTextFirstName.text.toString().trim()
+                val lastName = binding.editTextLastName.text.toString().trim()
+                val email = binding.editTextEmailAddress.text.toString().trim()
+                val password = binding.editTextPassword.text.toString().trim()
+                val confirmPassword = binding.editTextConfirmPassword.text.toString().trim()
+
+                if (firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
+                    enableButton(true)
+                } else {
+                    enableButton(false)
+                }
+            }
+        })
+
+        binding.editTextLastName.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+
+                val firstName = binding.editTextFirstName.text.toString().trim()
+                val lastName = binding.editTextLastName.text.toString().trim()
+                val email = binding.editTextEmailAddress.text.toString().trim()
+                val password = binding.editTextPassword.text.toString().trim()
+                val confirmPassword = binding.editTextConfirmPassword.text.toString().trim()
+
+                if (firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
+                    enableButton(true)
+                } else {
+                    enableButton(false)
+                }
+            }
+        })
+
+        binding.editTextEmailAddress.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+
+                val firstName = binding.editTextFirstName.text.toString().trim()
+                val lastName = binding.editTextLastName.text.toString().trim()
+                val email = binding.editTextEmailAddress.text.toString().trim()
+                val password = binding.editTextPassword.text.toString().trim()
+                val confirmPassword = binding.editTextConfirmPassword.text.toString().trim()
+
+                if (firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
+                    enableButton(true)
+                } else {
+                    enableButton(false)
+                }
+            }
+        })
+
+        binding.editTextPassword.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+
+                val firstName = binding.editTextFirstName.text.toString().trim()
+                val lastName = binding.editTextLastName.text.toString().trim()
+                val email = binding.editTextEmailAddress.text.toString().trim()
+                val password = binding.editTextPassword.text.toString().trim()
+                val confirmPassword = binding.editTextConfirmPassword.text.toString().trim()
+
+                if (firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
+                    enableButton(true)
+                } else {
+                    enableButton(false)
+                }
+            }
+        })
+
+        binding.editTextConfirmPassword.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+
+                val firstName = binding.editTextFirstName.text.toString().trim()
+                val lastName = binding.editTextLastName.text.toString().trim()
+                val email = binding.editTextEmailAddress.text.toString().trim()
+                val password = binding.editTextPassword.text.toString().trim()
+                val confirmPassword = binding.editTextConfirmPassword.text.toString().trim()
+
+                if (firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
+                    enableButton(true)
+                } else {
+                    enableButton(false)
+                }
+            }
+        })
     }
 }
