db5cced1fd08c46469b51d9040affa2b66269d6a
AbdulHafidh-AI <abdul_h20@mhs.unsyiah.ac.id>
2023-06-13 22:47:50 +0700

    docs: start to fill the documentation

--- Machine Learning/api/app.py
+++ Machine Learning/api/app.py
@@ -1,13 +1,19 @@
 import requests
 import json
 
-url = "http://127.0.0.1:8080/api/recommendation"
-#url = "https://recommendationrating-3boqvovjka-et.a.run.app"
-#url = "https://recommendationrating-3boqvovjka-et.a.run.app/api/recommendation"
+#url = "http://127.0.0.1:8080/api/recommendation" --> Must be run on local machine
+#url = "https://wismata-recommendation-api-3boqvovjka-uc.a.run.app/api/predict_rating" #this is for predict rating
+url = "https://wismata-recommendation-api-3boqvovjka-uc.a.run.app/api/recommendation" #this is for recommendation destination
 
 
 
 
-r = requests.get(url, params={'IdUser_Login': 1})
+r = requests.get(url, 
+
+#params={'IdUser_Login': 1} # this is for recommendation destination
+#params={"Category": 3, "Price": 2} # this is for predict rating
+
+
+)
 
 print(r.json())
